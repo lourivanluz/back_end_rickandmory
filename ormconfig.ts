0ed87f099a081b07d419757ce25e1cdedb051888
lourivanrluz <lourivan-rodrigues@hotmail.com>
2022-07-24 11:29:47 -0300

    fix: bug in deploy

--- ormconfig.ts
+++
@@ -1,8 +0,0 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-export const configDB: TypeOrmModuleOptions = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  synchronize: true,
-  entities: [__dirname + 'src/app/**/entities/*.entity{.js,.ts}'],
-};
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { configDB } from 'ormconfig';
+import { EpisodesEntity } from 'src/app/persons/entities/episode.entity';
+import { LocationsEntity } from 'src/app/persons/entities/location.entity';
+import { OriginEntity } from 'src/app/persons/entities/origin.entity';
+import { PersonsEntity } from 'src/app/persons/entities/persons.entity';
+import { UserEntity } from 'src/app/users/entities/users.entity';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(configDB)],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize: true,
+      entities: [
+        UserEntity,
+        EpisodesEntity,
+        LocationsEntity,
+        OriginEntity,
+        PersonsEntity,
+      ],
+    }),
+  ],
 })
 export class DatabaseModule {}
